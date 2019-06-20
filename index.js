1b9a764131e98c9e114e68257718ee6fac407700
hjrt <kjerte@gmail.com>
2019-06-20 22:19:11 +0200

    add creating namespace's name based on passed name

--- index.js
+++ index.js
@@ -2,25 +2,31 @@
 function install(Vue, options) {
     // Use "static" as the default name for the function/object
     // Changeable using options.name
-    var static_fn = 'static';
+    var static = {
+        fn: 'static',
+        namespace: function () {
+            return '$' + this.fn;
+        }
+    }
+
     if (options && options.name && typeof(options.name) === 'string') {
-        static_fn = options.name;
+        static.fn = options.name;
     }
     // Read more about option merge strategies in the official Vue.js docs
     if (typeof(Vue.config.optionMergeStrategies[static_fn]) !== 'function') {
         Vue.config.optionMergeStrategies[static_fn] = Vue.config.optionMergeStrategies.data;
     }
-    // Creates $static instance property if configured
+    // Creates an instance property based on the function/object name if should be namespaced
     // Empty by default
     if (options && options.namespaced) {
-        Vue.prototype.$static = {};
+        Vue.prototype[static.namespace()] = {};
     }
     // Idea: Properties that are added in the very first part of the Vue lifecycle
     //       don't get Vue's reactivity
     Vue.mixin({
         beforeCreate: function () {
             const vue_static = this.$options[static_fn];
-            const vue_static_destination = this.$static || this;
+            const vue_static_destination = this[static.namespace()] || this;
             if (vue_static && typeof(vue_static) === 'function') {
                 Object.assign(vue_static_destination, vue_static.apply(this));
             } else if (vue_static && typeof(vue_static) === 'object') {
